8b5919440f917d6bd76128f0b561104bee19fa52
PacGamer <PacGamer@users.noreply.github.com>
2018-02-04 18:03:43 -0500

    Rework status checking to support more than one channel
    Rework function flow to handle multiple stream statuses at a time. Discord icon will be set to onlne if at least one of the channels are online.

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var DISCORD_CLIENT_LOGIN = "";
 var DISCORD_TARGET_GUILD_ID = "";
 
 var TWITCH_BOT_USERNAME = "";
-var TWITCH_BOT_OAUTH = "";
-var TWITCH_TARGET_CHANNEL = "";
+var TWITCH_BOT_PASSWORD = "";
+var TWITCH_TARGET_CHANNEL = []; //One or more channels in format of an array. EX: ["twitch", "twitchpresents", "pacgamer"]
 var TWITCH_APP_CLIENT_ID = "";
 
 discordclient.on('ready', () => {
@@ -27,9 +27,9 @@ var options = {
 	},
 	identity: {
 		username: TWITCH_BOT_USERNAME,
-		password: TWITCH_BOT_OAUTH
+		password: TWITCH_BOT_PASSWORD
 	},
-	channels: ["#" + TWITCH_TARGET_CHANNEL]
+	channels: ["#" + TWITCH_BOT_USERNAME]
 };
 
 var twitchclient = new tmi.client(options);
@@ -37,21 +37,40 @@ var twitchclient = new tmi.client(options);
 // Connect the Twitch client to the server
 twitchclient.connect();
 
+//Run initial check and begin loop
 twitchclient.on("connected", function (address, port) {
-	streamStatus(TWITCH_TARGET_CHANNEL);
+	runChannels();
 	loop();
 });
+
+//Initialize some variables
+var lasticon = "online";
+var endofarray = false;
+var user_online_name = "";
+var user_online_found = false;
 
 function loop() {
 	setTimeout(function () {
 		loop();
-		streamStatus(TWITCH_TARGET_CHANNEL);
-	}, 60000);
+		runChannels();
+	}, 60000) //Loop time in milliseconds
 
 };
 
-var lasticon = "";
+//Check status of each channel in the array
+function runChannels() {
+	endofarray = false;
+	user_online_found = false;
+	for (var i = 0, len = TWITCH_TARGET_CHANNEL.length; i < len; i++) {
+		streamStatus(TWITCH_TARGET_CHANNEL[i]);
+		if (i == TWITCH_TARGET_CHANNEL.length - 1) {
+			endofarray = true;
+			discordIcon();
+		}
+	}
+};
 
+//Check individual stream status
 function streamStatus(channelname) {
 	twitchclient.api({
 		url: "https://api.twitch.tv/kraken/streams/" + channelname,
@@ -59,34 +78,45 @@ function streamStatus(channelname) {
 			"Client-ID": TWITCH_APP_CLIENT_ID
 		}
 	}, function (err, res, body) {
-		//console.log(body);
 
 		if (body["stream"] == null) {
 			// Channel is OFFLINE
-			console.log("Channel is offline");
-			// Edit the guild icon
-			if (lasticon != "offline") {
-				discordclient.guilds.get(DISCORD_TARGET_GUILD_ID).setIcon(fs.readFileSync('./offline.png'))
-				.then(updated => console.log('Updated the guild icon'))
-				.catch (console.error);
-
-				lasticon = "offline";
-			};
+			console.log("Channel \"" + channelname + "\" is offline");
 
 		} else {
 
 			// Channel is ONLINE
-			console.log("Channel is online");
-			// Edit the guild icon
-			if (lasticon != "online") {
-
-				discordclient.guilds.get(DISCORD_TARGET_GUILD_ID).setIcon(fs.readFileSync('./online.png'))
-				.then(updated => console.log('Updated the guild icon'))
-				.catch (console.error);
-
-				lasticon = "online";
-			};
+			console.log("Channel \"" + channelname + "\" is online");
+			user_online_found = true;
+			user_online_name = channelname;
 		}
 
-	});
-}
+	})
+};
+
+
+//Upload Discord icon when necessary
+function discordIcon() {
+	
+	if ((endofarray == true) && (user_online_found == false)) {
+		if (lasticon != "offline") {
+
+			discordclient.guilds.get(DISCORD_TARGET_GUILD_ID).setIcon(fs.readFileSync('./offline.png'))
+			.then(updated => console.log('Updated the guild icon'))
+			.catch (console.error);
+
+			lasticon = "offline";
+		}
+	}
+	if ((endofarray == true) && (user_online_found == true)) {
+		if (lasticon != "online") {
+
+			discordclient.guilds.get(DISCORD_TARGET_GUILD_ID).setIcon(fs.readFileSync('./online.png'))
+			.then(updated => console.log('Updated the guild icon'))
+			.catch (console.error);
+
+			lasticon = "online";
+		}
+	}
+
+};
